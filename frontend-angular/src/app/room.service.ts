402e48a29c90ab8b4b63e091750c62ed49cbbe56
Elon Lima <elon.blima@gmail.com>
2021-01-29 16:54:23 -0300

    feat: room details, create room, update room, list rooms

---
+++ frontend-angular/src/app/room.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+
+  constructor() { }
+
+  getRoomList() {}
+
+  deleteRoom(id: number) {
+
+  }
+
+  getRoom(id: number) {
+    
+  }
+  
+}
